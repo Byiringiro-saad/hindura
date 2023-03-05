4756d8a5c7c6935bd2cb5553517a96787ce9136e
Byiringiro Saad <byiringirosaad@gmail.com>
2023-03-05 19:20:13 +0200

    splash screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Splash from "./pages/splash";
 
 //pages
 import Upload from "./pages/upload";
 
 function App() {
+  const [showSplash, setShowSplash] = React.useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setShowSplash(false);
+    }, 4000);
+  }, []);
+
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Upload />} />
-      </Routes>
-    </Router>
+    <>
+      {showSplash ? (
+        <Splash />
+      ) : (
+        <Router>
+          <Routes>
+            <Route path="/" element={<Upload />} />
+          </Routes>
+        </Router>
+      )}
+    </>
   );
 }
 
--- src/pages/splash.jsx
+++ src/pages/splash.jsx
@@ -1,10 +1,90 @@
 import React from "react";
 import styled from "styled-components";
 
+//images
+import Logo from "../assets/Logo.png";
+
+//components
+import Background from "../components/background";
+
 const Splash = () => {
-  return <Container></Container>;
+  return (
+    <Background>
+      <Container>
+        <img src={Logo} alt="logo" />
+        <div className="para">
+          <p className="welcome" data-text="Welcome to">
+            Welcome to
+          </p>
+          <p className="name">Hindura</p>
+        </div>
+      </Container>
+    </Background>
+  );
 };
 
-const Container = styled.div``;
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  img {
+    width: 300px;
+  }
+
+  .para {
+    width: 100%;
+    height: 50px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+
+    p.name {
+      font-size: 3em;
+      margin: 20px 0 0 6px;
+      font-weight: 800;
+      color: var(--bright);
+    }
+
+    p.welcome {
+      font-size: 3em;
+      margin: 20px 0 0 15px;
+      font-weight: 800;
+      color: var(--dark);
+      position: relative;
+      -webkit-text-stroke: 0.4px var(--white);
+
+      ::before {
+        content: attr(data-text);
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        color: var(--white);
+        overflow: hidden;
+        -webkit-text-stroke: 0vw var(--dark);
+        animation: animate 5s ease-in;
+      }
+
+      @keyframes animate {
+        0%,
+        10%,
+        100% {
+          width: 0;
+        }
+
+        70%,
+        90% {
+          width: 100%;
+        }
+      }
+    }
+  }
+`;
 
 export default Splash;
